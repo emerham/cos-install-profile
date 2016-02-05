5a5af8ed5e4ea2542904ae2741c38bf8cc212d94
Matthew Brabham <matthew.brabham@oregonstate.edu>
2016-02-05 13:46:44 -0800

    SQL Updates for blocks

---
+++ sql_updates/live-feeds-blocks.sql
@@ -0,0 +1,4 @@
+UPDATE block SET region="main_first", status="1" where module="live_feeds" AND delta="3" AND theme="doug_fir";
+UPDATE block SET region="main_second", status="1" where module="live_feeds" AND delta="4" AND theme="doug_fir";
+UPDATE block SET region="main_third", status="1" where module="live_feeds" AND delta="5" AND theme="doug_fir";
+UPDATE block SET region="main_first", status="1", weight="5" where module="live_feeds" AND delta="6" AND theme="doug_fir";
